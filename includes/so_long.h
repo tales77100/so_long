0f58843bae3882a6c9fd7e55f20060896914b458
Jamy Santini <jsantini@f1r11s8.clusters.42paris.fr>
2025-12-04 16:52:24 +0100

    asdasddas

--- includes/so_long.h
+++ includes/so_long.h
@@ -13,6 +13,29 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+/*=======struct=======*/
+
+/* window thing */
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+/***************/
+
+/* data */
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+/*******/
+
+/*====================*/
+
 # include "../libft/libft.h"
 # include "../mlx/mlx.h"
 
--- srcs/main.c
+++ srcs/main.c
@@ -12,30 +12,56 @@
 
 #include "so_long.h"
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
 
 int	close_w(int keycode, t_vars *vars)
 {
 	if (keycode == 65307)
 		mlx_destroy_window(vars->mlx, vars->win);
+	exit(1);
 	return (0);
 }
 
 int	close_w_cross(t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->win);
+	exit(1);
 	return (0);
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
 
 int	main(void)
 {
 	t_vars	vars;
+	t_data	img;
+	int	x;
+	int	y;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	vars.win = mlx_new_window(vars.mlx, 500, 500, "So-long");
+	
+	img.img = mlx_new_image(vars.mlx, 500, 500);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	
+	x = 0;
+	y = 0;
+	while (x < 10)
+		my_mlx_pixel_put(&img, x++, y, 0x00FF0000);
+	while (y < 10)
+		my_mlx_pixel_put(&img, x, y++, 0x00FF0000);
+	while (x > 0)
+		my_mlx_pixel_put(&img, x--, y, 0x00FF0000);
+	while (y > 0)
+		my_mlx_pixel_put(&img, x, y--, 0x00FF0000);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	mlx_hook(vars.win, 2, 1L<<0, close_w, &vars);
 	mlx_hook(vars.win, 17, 0, close_w_cross, &vars);
 	mlx_loop(vars.mlx);
